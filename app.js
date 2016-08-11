d3116edfe3cc84ef5c2352ed0a88f28a56b7ec03
Walter <relw1wa-github@yahoo.de>
2016-08-11 12:49:46 +0200

    Add index.html and middleware for static files

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var port = process.env.PORT || 3000;
 var mongo = require('./mongo.js');
 
+app.use('/', express.static(path.join(__dirname, 'public')));
+
 app.listen(port, function(err, res) {
   if (err) {
     console.log('Error happened during server startup:', err);
