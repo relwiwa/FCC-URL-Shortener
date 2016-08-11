bc3258c92db845d752bc8b92c2f00d7bed501633
Walter <relw1wa-github@yahoo.de>
2016-08-11 14:02:54 +0200

    Add MongoDB connectivity using connection pooling

--- app.js
+++ app.js
@@ -6,11 +6,20 @@ var mongo = require('./mongo.js');
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
-app.listen(port, function(err, res) {
-  if (err) {
-    console.log('Error happened during server startup:', err);
-  }
-  else {
-    console.log('Server started successfully');
-  }
+/*  Following MongoDB's suggested practice for connection pooling:
+    - create DB connection during startup
+    - put app.listen after DB connection is established
+    http://blog.mlab.com/2013/11/deep-dive-into-connection-pooling */
+mongo.initDB().then(function(db) {
+  console.log('Connection to database successfully established');
+  app.listen(port, function(err, res) {
+    if (err) {
+      console.log('Error happened during server startup:', err);
+    }
+    else {
+      console.log('Server started successfully');
+    }
+  });
+}, function(err) {
+  console.log('Connection to database could not be established', err);
 });
--- mongo.js
+++ mongo.js
@@ -1,2 +1,69 @@
 var mongo = require('mongodb').MongoClient;
 var dbPath = process.env.MONGOLAB_URI_URL_SHORTENER;
+var connectedDB = null;
+var connectedColls = {};
+
+module.exports = {
+  'initDB': initDB,
+  'getDB': getDB,
+  'getColl': getColl
+}
+
+function initDB() {
+  var promise = new Promise(function(resolve, reject) {
+    if (connectedDB === null) {
+      console.log('Initial retrieval of DB');
+      mongo.connect(dbPath).then(function(db) {
+        resolve(db);
+      }, function(err) {
+        reject(err);
+      });
+    }
+    else {
+      console.log('Reusing already established DB connection');
+      resolve(connectedDB);
+    }
+  });
+  return promise;
+}
+
+function getDB() {
+  var promise = new Promise(function(resolve, reject) {
+    if (connectedDB !== null) {
+      resolve(connectedDB);
+    }
+    else {
+      initDB().then(function(db) {
+        resolve(db);
+      }, function(err) {
+        reject(err);
+      });
+    }
+  });
+  return promise;
+}
+
+function getColl(coll) {
+  var promise = new Promise(function(resolve, reject) {
+    if (connectedColls[coll]) {
+      console.log('Reusing collection ' + coll);
+      resolve(connectedColls[coll]);
+    }
+    else {
+      console.log('Initial retrieval of collection ' + coll);
+      getDB().then(function(db) {
+        // Collection function only works with callback, not with promise
+        db.collection(coll, function(err, result) {
+          if (err !== null) {
+            reject(err);
+          }
+          else {
+            connectedColls[coll] = result;
+            resolve(connectedColls[coll]);
+          }
+        });
+      });
+    }
+  });
+  return promise;
+}
